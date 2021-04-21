96887b0f6e1ea64b403cd8f15f047abe30792d82
ErickQuintana <Erickquintana1997@gmail.com>
2021-04-21 07:01:05 +0000

    added terrible work to repository

---
+++ gc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+# Use all three formatting methods
+
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT' 
+gc_content= 0# feel free to change
+for i in range(len(dna)):
+    if dna[i] == 'G'or dna[i] == 'C':
+        gc_content += 1 
+print(f'{gc_content/len(dna):.2f}')
+
+        
+    
+    
+
+"""
+python3 gc.py
+0.42
+0.42
+0.42
+"""
